8dce544a57b9a2b6e11eb0082ce92138cc25c2ad
Michael Spencer <michael.spencer@q2ebanking.com>
2019-06-17 18:43:28 -0500

    add styled-components

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import styled from 'styled-components'
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>
-        </Text>   
-      </View>
-    );
+      <Container>
+
+      </Container>
+    )
   }
 }
+
+const Container = styled.View`
+  background: red;
+  width: 100%;
+  height: 100%;
+`
 
 // const styles = StyleSheet.create({
 //   container: {
@@ -29,4 +35,4 @@ export default class App extends Component {
 //     color: '#333333',
 //     marginBottom: 5,
 //   },
-// });
+// })
